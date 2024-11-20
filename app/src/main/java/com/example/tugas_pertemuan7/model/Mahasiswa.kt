8ce7668bdf144de90fd7498c260347893674de3e
defarrel <Defarrel>
2024-11-20 19:06:13 +0700

    Menambahkan data class mahasiswa

---
+++ app/src/main/java/com/example/tugas_pertemuan7/model/Mahasiswa.kt
@@ -0,0 +1,9 @@
+package com.example.tugas_pertemuan7.model
+
+data class Mahasiswa(
+    val nama: String = "",
+    val gender: String = "",
+    val alamat: String = "",
+    val email: String = "",
+    val noTelpon: String = ""
+)
