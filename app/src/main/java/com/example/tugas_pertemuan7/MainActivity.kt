6a6c63a1b91ecd2abb6207b589be154b5687d402
defarrel <Defarrel>
2024-11-20 19:19:20 +0700

    Memanggil function NavigationControl di MainActivity

--- app/src/main/java/com/example/tugas_pertemuan7/MainActivity.kt
+++ app/src/main/java/com/example/tugas_pertemuan7/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tugas_pertemuan7.ui.theme.Tugas_Pertemuan7Theme
+import com.example.tugas_pertemuan7.ui.viewmodel.NavigationControl
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Tugas_Pertemuan7Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    NavigationControl(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +30,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Tugas_Pertemuan7Theme {
-        Greeting("Android")
-    }
-}
