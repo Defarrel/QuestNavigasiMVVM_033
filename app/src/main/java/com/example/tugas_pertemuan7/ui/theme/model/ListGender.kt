32cfb7c5f36d786b81e8433ea971fd2a23e2905c
defarrel <Defarrel>
2024-11-20 19:01:26 +0700

    Menambahkan file object bernama ListGender dengan memasukan juga 2 variabel

---
+++ app/src/main/java/com/example/tugas_pertemuan7/ui/theme/model/ListGender.kt
@@ -0,0 +1,10 @@
+package com.example.tugas_pertemuan7.ui.theme.model
+
+import com.example.tugas_pertemuan7.R
+
+object ListGender {
+    val ListGender = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
